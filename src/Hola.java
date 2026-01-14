4cbb2e7e26f37752c1524a19cdb38eb3011e9277
Almudena Jimenez Aguilar <ajimagu1502@g.educaand.es>
2026-01-14 13:37:19 +0100

    Cambio rama 2

--- src/Hola.java
+++ src/Hola.java
@@ -2,6 +2,8 @@ public class Hola {
     public static void main(String[] args) {
         System.out.println("hola caracola");
         System.out.println("hola caracola");
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
 
     }
 }
